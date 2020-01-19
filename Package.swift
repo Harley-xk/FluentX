2dc5dd4d50081f28d9a05431a5a22bd69d0cfe26
Harley-xk <harley.gb@foxmail.com>
2020-01-19 19:38:27 +0800

    update dependencies from 3.0.0

--- Package.swift
+++ Package.swift
@@ -13,14 +13,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-alpha.3"),
-         .package(url: "https://github.com/vapor/fluent-kit", from: "1.0.0-alpha.3.1"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", .upToNextMajor(from: "3.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FluentX",
-            dependencies: ["FluentSQLiteDriver", "FluentKit"]),
+            dependencies: ["FluentSQLite"]),
     ]
 )
